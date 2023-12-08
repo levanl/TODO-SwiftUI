1c4d22bf991f372b24107bf979b63330ec9bdf95
Levan Loladze <levan.loladzew@gmail.com>
2023-12-08 10:05:47 +0400

    first commit

---
+++ TODO App/TODO App/TODO_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_AppApp.swift
+//  TODO App
+//
+//  Created by Levan Loladze on 08.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TODO_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TodoMainView()
+        }
+    }
+}
---
+++ TODO App/TODO App/TodoMainView.swift
@@ -0,0 +1,24 @@
+//
+//  TodoMainView.swift
+//  TODO App
+//
+//  Created by Levan Loladze on 08.12.23.
+//
+
+import SwiftUI
+
+struct TodoMainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TodoMainView()
+}
